96299aeef4f29e826bf561904f031303c860bb1c
Zohar Arad <zohar@zohararad.com>
2012-08-28 19:40:25 +0300

    added post model and controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,24 @@
 class PostsController < ApplicationController
 
   def index
-
+    page = params[:page] || 1
+    @posts = Post.page(page).per(10)
+    respond_to do |format|
+      format.html
+      format.json {
+        render :json => @posts.to_jade
+      }
+    end
   end
 
   def show
-
+    @post = Post.find params[:id]
+    respond_to do |format|
+      format.html
+      format.json {
+        render :json => @post.to_jade
+      }
+    end
   end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,9 @@
 class Post
   include Mongoid::Document
   include Mongoid::Timestamps
+  include Jader::Serialize
+
+  jade_serializable :title, :content, :comments, :merge => true
 
   field :title, type: String
   field :content, type: String
