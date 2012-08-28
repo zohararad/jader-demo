aa3d480de007c2badbbb29c2e69116744b06b675
Zohar Arad <zohar@zohararad.com>
2012-08-28 19:42:20 +0300

    added spine views to view render path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :prepend_view_paths
+
+  private
+
+  # Adds Client-Side views to render path
+  def prepend_view_paths
+    prepend_view_path Rails.root.join('app', 'assets', 'javascripts', 'app', 'views')
+  end
+
 end
