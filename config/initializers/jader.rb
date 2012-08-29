8b477f02cb325193706791e3577161ad7c04670f
Zohar Arad <zohar@zohararad.com>
2012-08-29 07:51:41 +0300

    updated to jader 0.0.5 and updated configuration to support views_path

--- config/initializers/jader.rb
+++ config/initializers/jader.rb
@@ -1,5 +1,6 @@
 Jader.configure do |config|
   config.mixins_path = Rails.root.join('app','assets','javascripts','app', 'mixins')
+  config.views_path = Rails.root.join('app', 'assets', 'javascripts', 'app', 'views')
   # wait for assets to be ready
   Rails.application.config.after_initialize do
     # include javascripts available only from asset pipeline
