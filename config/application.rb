43d6417fb8093eda4916679aac7fc1396bdb2b98
Zohar Arad <zohar@zohararad.com>
2012-08-29 12:37:44 +0300

    fix production errors due to compiled assets

--- config/application.rb
+++ config/application.rb
@@ -61,7 +61,6 @@ module JaderExample
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    config.assets.precompile += ['router.js']
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
